bc0c47deaf3a93cf3a84808cf7a474cc694c7284
VINICIUS GOBBO <144715573+vinigobbo@users.noreply.github.com>
2026-09-22 16:33:57 -0300

    Analise de IVVB11 com python para praticar

--- Analise de dados.py
+++ Analise de dados.py
@@ -1,9 +1,21 @@
-precos = [10.0, 10.5, 10.2, 11.0, 12.0, 12.9, 11.8, 10.5]
+etf = [443.90, 440.60, 442.50, 443.66, 444.30, 445.61, 448.06]
 retornos = []
-for i in range(1, len(precos)):
-    retorno = precos[i] / precos[i - 1] - 1
-    retornos.append(retorno)
-print(retornos)    
+desvios = []
+for i in range(1, len(etf)):
+  retorno = etf[i] / etf[i - 1] - 1
+  retornos.append(round(retorno * 100, 2),)
+print("Retornos:", retornos)
 
-media = sum(retornos)/len(retornos)
-print(round(media * 100, 2), "%")
+media = sum(retornos) / len(retornos)
+print("Media:", round(media, 2), "%")
+
+soma_quadrados = 0
+
+for r in retornos:
+  desvio = r - media
+  soma_quadrados = soma_quadrados + desvio ** 2
+
+print(round(soma_quadrados, 5))
+
+volatilidade = (soma_quadrados / len(retornos)) ** 0.5
+print(round(volatilidade, 2), "%")
