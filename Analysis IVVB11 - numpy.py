3329fe5314ad9bb63138323627a9c5dc6d4ae959
VINICIUS GOBBO <vinicgobbo@gmail.com>
2026-09-24 12:03:33 -0300

    Analysis IVVB11 NumPy

--- Analysis IVVB11 - numpy.py
+++ Analysis IVVB11 - numpy.py
@@ -8,8 +8,20 @@ retorno = hoje / ontem - 1
 retorno = retorno * 100
 print("Retornos:", np.round(retorno, 2))
 
+retorno_max = np.max(retorno)
+retorno_min = np.min(retorno)
+
+print("Maior retorno:", np.round(retorno_max, 2), "%")
+print("Menor retorno:", np.round(retorno_min, 2), "%")
+
+dias_alta = np.sum(retorno > 0)
+
+print("Dias de alta:", np.round(dias_alta))
+
+
 media = retorno.mean()
 volatilidade = retorno.std() 
 
 print("Media:", np.round(media, 2), "%")
 print("Volatilidade:", np.round(volatilidade, 2), "%")
+
